efb590339724b269feed8dc4cfe20c6bad8f9aa4
Matthew Chan <matt@awakenetworks.com>
2017-10-26 14:29:29 -0700

    init

---
+++ src/Control/Lens/Yotta.hs
@@ -0,0 +1,17 @@
+module Control.Lens.Yotta where
+import Data.Functor.Const
+import Data.Functor.Identity
+
+type Lens s t a b = forall f. Functor f -> (a -> f b) -> s -> f t
+
+lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens getter setter afb s = fmap setter (afb (getter s))
+
+get :: Lens s t a b -> s -> a
+get lens = getIdentity . lens Identity
+
+set :: Lens s t a b -> b -> s -> t
+set lens b = getConst . lens (Const b)
+
+over :: Lens s t a b -> (a -> b) -> s -> t
+over lens f = getConst . lens (Const . f)

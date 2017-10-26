e8e3bce0afafef14303c3aaacb6493068ce6eb3b
Matthew Chan <matt@awakenetworks.com>
2017-10-26 14:44:18 -0700

    derp

--- src/Control/Lens/Yocto.hs
+++ src/Control/Lens/Yocto.hs
@@ -1,17 +1,18 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Lens.Yocto where
 import Data.Functor.Const
 import Data.Functor.Identity
 
-type Lens s t a b = forall f. Functor f -> (a -> f b) -> s -> f t
+type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
 
 lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
-lens getter setter afb s = fmap setter (afb (getter s))
+lens getter setter afb s = fmap (setter s) (afb (getter s))
 
 get :: Lens s t a b -> s -> a
-get lens = getIdentity . lens Identity
+get l = getConst . l Const
 
 set :: Lens s t a b -> b -> s -> t
-set lens b = getConst . lens (Const b)
+set l b = runIdentity . l (const (Identity b))
 
 over :: Lens s t a b -> (a -> b) -> s -> t
-over lens f = getConst . lens (Const . f)
+over l f = runIdentity . l (Identity . f)
